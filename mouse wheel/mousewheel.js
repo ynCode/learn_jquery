95e560ad8c5470ce1be1482111db75066ac04d25
ynCode <younixiao@qq.com>
2015-01-23 15:09:45 +0800

    add addClassActive()

--- mouse wheel/mousewheel.js
+++ mouse wheel/mousewheel.js
@@ -1,6 +1,21 @@
 jQuery(document).ready(function($) {
+
+    function addClassActive(thisobj) {
+
+        $('a').removeClass('active');
+        console.log(thisobj.prop('tagName'));
+        if (thisobj.prop('tagName') == 'A') {
+            linkNum = thisobj.index();
+        } else if (thisobj.prop('tagName') == 'DIV') {
+            linkNum = thisobj.index() + 1;
+        };
+        $('header nav').children('a').eq(linkNum).addClass('active');
+        $('.nav-side').children('a').eq(linkNum).addClass('active');
+    }
+
     // click a to change div
     $('a').click(function() {
+        addClassActive($(this));
         var id = $(this).attr('href');
         $('.warp div').css('display', 'none').removeClass('mouse-on');
         $(id).fadeIn('slow', function() {
@@ -10,34 +25,49 @@ jQuery(document).ready(function($) {
 
     // mouse wheel event
     $('body').mousewheel(function(event) {
+
         // if mouse wheel down
         if (event.deltaY == -1) {
+
             var end = $('.mouse-on').next().length;
+
             // if it is not last
             if (end == 1) {
+
                 $('.mouse-on').hide(function() {
                     $(this).removeClass('mouse-on');
                     $(this).next('.warp div').addClass('mouse-on').show('slow');
+                    addClassActive($(this));
                 });
+
             } else if (end == 0) { // if it is last
+
                 $('.mouse-on').hide(function() {
                     $(this).removeClass('mouse-on');
                     $('.warp div:first-child').addClass('mouse-on').show('slow');
+                    addClassActive($(this));
                 });
+
             };
 
         } else if (event.deltaY == 1) { // if mouse wheel on
+
             var start = $('.mouse-on').prev().length;
+
             // if it is not first
             if (start == 1) {
+
                 $('.mouse-on').hide(function() {
                     $(this).removeClass('mouse-on');
                     $(this).prev().addClass('mouse-on').show('slow');
+                    addClassActive($(this));
                 });
             } else if (start == 0) { // if it is first
+
                 $('.mouse-on').hide('slow', function() {
                     $(this).removeClass('mouse-on');
                     $('.warp div:last-child').addClass('mouse-on').show('slow');
+                    addClassActive($(this));
                 });
 
             };
